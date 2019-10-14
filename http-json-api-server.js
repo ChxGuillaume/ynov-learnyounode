255f32e18dabbd468569c7188a0268ad3aab975b
Chateauroux Guillaume <guillaume.chateauroux@gmail.com>
2019-10-14 16:44:13 +0200

    added error managment

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -3,19 +3,37 @@ const [node, path, port] = process.argv;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = new URL(req.url, 'http://localhost/');
-    const date = new Date(parsedUrl.searchParams.get('iso'));
+    const iso = parsedUrl.searchParams.get('iso');
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    if (req.url.match('/api/parsetime')) {
-        res.write(JSON.stringify({
-            hour: date.getHours(),
-            minute: date.getMinutes(),
-            second: date.getSeconds(),
-        }));
-    } else if (req.url.match('/api/unixtime')) {
-        res.write(JSON.stringify({
-            unixtime: date.getTime()
-        }));
+    if (!iso) {
+        res.writeHead(400);
+        res.write('No ISO parameter specified.');
+        res.end();
+    } else {
+        const date = new Date(iso);
+
+        if (!date) {
+            res.writeHead(400);
+            res.write('Bad date format.');
+            res.end();
+        } else {
+            if (parsedUrl.pathname === '/api/parsetime') {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({
+                    hour: date.getHours(),
+                    minute: date.getMinutes(),
+                    second: date.getSeconds(),
+                }));
+            } else if (parsedUrl.pathname === '/api/unixtime') {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({
+                    unixtime: date.getTime()
+                }));
+            } else {
+                res.writeHead(404);
+                res.write('Not found.');
+            }
+            res.end();
+        }
     }
-    res.end();
 }).listen(port);
